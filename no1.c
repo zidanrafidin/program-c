0d050fdb52794333cfb748d2cdb9c03432f26bdc
zidan <zidanrafidin@gmail.com>
2025-09-07 14:37:45 +0700

    first commit

---
+++ no1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\nNama : Zidan Rafidin Akhmad\n");
+    printf("NRP : 3125600020\n");
+    printf("Kelas : 1 D4 IT A\n");
+    return 0;
+}
---
+++ no2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a=1024, b=4096;
+    printf("hasil dari %d + %d = %d\n", a, b, a+b);
+
+    return 0;
+}
