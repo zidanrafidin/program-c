dbf2649d77464b1bbd9f7cf9c428ffcab5572410
zidan <zidanrafidin@gmail.com>
2025-09-07 16:25:22 +0700

    add no5

---
+++ no5.c
@@ -0,0 +1,10 @@
+// Menampilkan nilai sebuah bilangan float dengan tanpa menentukan format
+// tampilannya (default)
+
+#include <stdio.h>
+int main(){
+    float a = 13.75;
+    printf("nilai float a = %f\n",a);
+
+    return 0;
+}
